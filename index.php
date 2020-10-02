120c452ac010a6a17a3789b5b21d383ebeac2f5a
Luciano Delfino <luciano.dlf@gmail.com>
2020-10-02 10:46:31 -0300

    add logo and more change visual

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <title>SPH - Bertone</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,13 +29,13 @@
 
     <!-- Top container -->
     <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
-        <!-- <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i> Menu</button> -->
-        <span class="w3-bar-item w3-right">SPH - Premoldeados Bertone</span>
+        <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" style="margin-top: 5px;" onclick="w3_open();"><i class="fa fa-bars"></i> Menu</button>
+        <img src="/assets/logo-bertone-v10.png" class="w3-bar-item w3-right" style="height: 60px;">
     </div>
 
     <!-- Sidebar/menu -->
-    <!-- <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
-        <div class="w3-container w3-row">
+    <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:200px;" id="mySidebar"><br>
+        <!-- <div class="w3-container w3-row">
             <div class="w3-col s4">
                 <img src="/w3images/avatar2.png" class="w3-circle w3-margin-right" style="width:46px">
             </div>
@@ -46,43 +46,37 @@
                 <a href="#" class="w3-bar-item w3-button"><i class="fa fa-cog"></i></a>
             </div>
         </div>
-        <hr>
+        <hr> -->
         <div class="w3-container">
-            <h5>Dashboard</h5>
+            <h5>Menu</h5>
         </div>
         <div class="w3-bar-block">
-            <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i> Close Menu</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-users fa-fw"></i> Overview</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i> Views</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i> Traffic</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bullseye fa-fw"></i> Geo</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-diamond fa-fw"></i> Orders</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bell fa-fw"></i> News</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bank fa-fw"></i> General</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-history fa-fw"></i> History</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i> Settings</a><br><br>
+            <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i> Cerrar Menu</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-tasks fa-fw"></i> Procesamiento</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i> Configuracion</a>
         </div>
-    </nav> -->
+    </nav>
 
 
     <!-- Overlay effect when opening sidebar on small screens -->
-    <!-- <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div> -->
+    <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
     <!-- !PAGE CONTENT! -->
-    <div class="w3-main" style="margin-left:auto;margin-top:43px;">
+    <div class="w3-main" style="margin-left:200px;margin-top:43px;">
 
         <!-- Header -->
         <header class="w3-container" style="padding-top:22px">
-            <h5 style="margin-left:30px;"><b><i class="fa fa-dashboard"></i> Procesamiento de horas</b></h5>
+            <h5 style="margin-left:30px;"><b><i class="fa fa-tasks"></i> Procesamiento de horas</b></h5>
         </header>
 
         <div class="w3-row-padding w3-margin-bottom">
             <div id="msgResponseError" class="w3-panel w3-red w3-round-large" style="display: none; padding:10px;"></div>
             <div id="msgResponseSuccess" class="w3-panel w3-green w3-round-large" style="display: none; padding:10px;"></div>
 
-            <form action="uploadFile.php" method="POST" id="formUploadFile" enctype="multipart/form-data" class="w3-container w3-light-grey w3-text-blue w3-margin">
-                <input class="w3-input w3-border w3-round-large" type="file" id="file" name="file" accept="xlsx"><br>
-                <input class="w3-btn w3-light-blue w3-round-large" type="submit" value="Cargar">
+            <form lang="es" action="uploadFile.php" method="POST" id="formUploadFile" enctype="multipart/form-data" class="w3-container w3-light-grey w3-text-blue w3-margin">
+                
+                <input class="w3-input w3-border w3-round-large" lang="es" type="file" id="file" name="file" accept="xlsx"><br>
+                <input class="w3-btn w3-light-blue w3-round-large" type="submit" value="Procesar">
                 <img id="loadingGif" src="assets/Infinity-1s-200px.gif" style="width:50%;max-width:50px;display: none;" class="w3-round" alt="loading">
                 <hr>
 
@@ -191,13 +185,13 @@
 
         // --------------------------------------------------------------------
         // Get the Sidebar
-        // var mySidebar = document.getElementById("mySidebar");
+        var mySidebar = document.getElementById("mySidebar");
 
         // Get the DIV with overlay effect
-        // var overlayBg = document.getElementById("myOverlay");
+        var overlayBg = document.getElementById("myOverlay");
 
         // Toggle between showing and hiding the sidebar, and add overlay effect
-        /* function w3_open() {
+        function w3_open() {
             if (mySidebar.style.display === 'block') {
                 mySidebar.style.display = 'none';
                 overlayBg.style.display = "none";
@@ -205,13 +199,13 @@
                 mySidebar.style.display = 'block';
                 overlayBg.style.display = "block";
             }
-        } */
+        }
 
         // Close the sidebar with the close button
-        /* function w3_close() {
+        function w3_close() {
             mySidebar.style.display = "none";
             overlayBg.style.display = "none";
-        } */
+        }
     </script>
 
 </body>
