fcebf73aa9756a41f9f2428b8cb9c4ff96fda847
Luciano Delfino <luciano.dlf@gmail.com>
2020-10-30 20:24:38 -0300

    add export summary hours by user

--- header/export_excel.php
+++ header/export_excel.php
@@ -192,6 +192,9 @@ function exportExcel($hours_data, $export_file, $absences_summary = NULL)
 	// Set array data for write excel
 	$arr_data_users = [];
 
+	// Set array data for summary hours by each user
+	$arr_summary_hours = [];
+
 	foreach ($hours_data as $user_id => $user) {
 
 		foreach ($user['INTERVALS'] as $interval_id => $interval) {
@@ -215,6 +218,18 @@ function exportExcel($hours_data, $export_file, $absences_summary = NULL)
 				'N' => (string) convertMinutosToHoursAndMinutes($interval['RESULT']['total']),
 				'O' => (string) returnObservationAlert($interval),
 				'P' => ($interval['input']['is_date_fixed']) ? "SI" : "",
+			];
+
+			$arr_summary_hours[$user_id] = [
+				'A' => (string) $user_id,
+				'B' => (string) $interval['input']['name'],
+				'C' => $arr_summary_hours[$user_id]['C'] + $interval['RESULT']['diurnas_n'],
+				'D' => $arr_summary_hours[$user_id]['D'] + $interval['RESULT']['diurnas_e'],
+				'E' => $arr_summary_hours[$user_id]['E'] + $interval['RESULT']['nocturnas_n'],
+				'F' => $arr_summary_hours[$user_id]['F'] + $interval['RESULT']['nocturnas_e'],
+				'G' => $arr_summary_hours[$user_id]['G'] + $interval['RESULT']['h100'],
+				'H' => $arr_summary_hours[$user_id]['H'] + ($interval['input']['is_feriado'] || $interval['output']['is_feriado']) ? 1 : 0,
+				'I' => $arr_summary_hours[$user_id]['I'] + $interval['RESULT']['total'],
 			];
 		}
 	}
@@ -259,7 +274,7 @@ function exportExcel($hours_data, $export_file, $absences_summary = NULL)
 		exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data);
 	}
 
-
+	exportSummaryHours($spreadsheet, $arr_summary_hours);
 
 
 	$write = IOFactory::createWriter($spreadsheet, 'Xlsx');
@@ -279,10 +294,10 @@ function exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data
 	$worksheetSummaryAbsences = new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, 'RESUMEN INASISTENCIAS');
 
 	// Agrega la nueva worksheet con indice 1
-	$spreadsheet->addSheet($worksheetSummaryAbsences, 1);
+	$spreadsheet->addSheet($worksheetSummaryAbsences, 2);
 
 	// Activamos la nueva worksheet para trabajar sobre ella
-	$spreadsheet->setActiveSheetIndex(1);
+	$spreadsheet->setActiveSheetIndex(2);
 
 	/**
 	 *
@@ -404,7 +419,7 @@ function exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data
 			$spreadsheet->getActiveSheet()->setCellValue("{$column}{$row_data}", $value);
 
 			$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->applyFromArray($style_data_array);
-			
+
 			if ($column === 'D' && $value === 'IT') {
 
 				// Set font color
@@ -416,7 +431,7 @@ function exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data
 
 				// Set bold
 				$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->getFont()->setBold(TRUE);
-			}else if ($column === 'D' && validateTypeJournal(DateTime::createFromFormat('d/m/Y', $row['C'])->format('w'), TRUE) > $value) {
+			} else if ($column === 'D' && validateTypeJournal(DateTime::createFromFormat('d/m/Y', $row['C'])->format('w'), TRUE) > $value) {
 
 				// Set font color
 				// $spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_WHITE);
@@ -428,7 +443,7 @@ function exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data
 				// Set bold
 				$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->getFont()->setBold(TRUE);
 			}
-			
+
 			if ($column === 'D' && $value === 'FF') {
 				// Set background color
 				$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
@@ -467,5 +482,126 @@ function exportSummaryAbsencesExcel($spreadsheet, $absences_summary, $hours_data
 	$spreadsheet->getActiveSheet()->setCellValue("H5", "Cantidad de horas registradas");
 
 	// Volvemos a ctivar la worksheet principal
+	$spreadsheet->setActiveSheetIndex(0);
+}
+
+
+
+
+function exportSummaryHours($spreadsheet, $arr_summary_hours)
+{
+
+	//Creamos nueva worksheet
+	$worksheetSummaryHours = new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, 'RESUMEN HORAS');
+
+	// Agrega la nueva worksheet con indice 1
+	$spreadsheet->addSheet($worksheetSummaryHours, 1);
+
+	// Activamos la nueva worksheet para trabajar sobre ella
+	$spreadsheet->setActiveSheetIndex(1);
+
+	/**
+	 *
+	 * ========================== Prepare value title for export ================================
+	 * 
+	 */
+
+	$titles = [
+		'A' => 'CU',
+		'B' => 'NOMBRE',
+		'C' => 'DIURNAS',
+		'D' => 'DIURNAS E',
+		'E' => 'NOCTURNAS',
+		'F' => 'NOCTURNAS E',
+		'G' => 'H100',
+		'H' => 'FERIADOS',
+		'I' => 'TOTAL',
+	];
+
+	$row_title = 1;
+
+	$style_title_array = [
+		'borders' => [
+			'allBorders' => [
+				'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+				'color' => ['argb' => \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK],
+			],
+		],
+		'font' => [
+			'bold' => true,
+			'color' => ['argb' => \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_WHITE]
+		],
+		'alignment' => [
+			'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+		],
+		'fill' => [
+			'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+			'startColor' => [
+				'argb' => \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK,
+			],
+		],
+	];
+
+
+	foreach ($titles as $column => $value) {
+
+		// Set columns autozise for title
+		$spreadsheet->getActiveSheet()->getColumnDimension("{$column}")->setAutoSize(TRUE);
+
+		// Set style from array
+		$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_title}")->applyFromArray($style_title_array);
+
+		// Set value
+		$spreadsheet->getActiveSheet()->setCellValue("{$column}{$row_title}", $value);
+	}
+
+	/**
+	 *
+	 * ========================== Prepare value data for export ================================
+	 * 
+	 */
+
+	// Set style cells of data
+	$style_data_array = [
+		'borders' => [
+			'allBorders' => [
+				'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+				'color' => ['argb' => \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK],
+			],
+		],
+		'font' => [
+			'bold' => false,
+			'color' => ['argb' => \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK]
+		],
+		'alignment' => [
+			'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+		]
+	];
+
+
+	foreach ($arr_summary_hours as $user_id => $data) {
+		$data['C'] = ($data['C'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['C']) : 0;
+		$data['D'] = ($data['D'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['D']) : 0;
+		$data['E'] = ($data['E'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['E']) : 0;
+		$data['F'] = ($data['F'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['F']) : 0;
+		$data['G'] = ($data['G'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['G']) : 0;
+		$data['I'] = ($data['I'] !== 0) ? (string) convertMinutosToHoursAndMinutes($data['I']) : 0;
+
+		$arr_summary_hours[$user_id] = $data;
+	}
+
+
+	$row_data = 2;
+
+	foreach ($arr_summary_hours as $row) {
+		foreach ($row as $column => $value) {
+
+			$spreadsheet->getActiveSheet()->setCellValue("{$column}{$row_data}", $value);
+
+			$spreadsheet->getActiveSheet()->getStyle("{$column}{$row_data}")->applyFromArray($style_data_array);
+		}
+		$row_data++;
+	} 
+
 	$spreadsheet->setActiveSheetIndex(0);
 }
